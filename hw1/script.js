4bbd5339e40ca6cbedd79ebf950cc74fa04fe032
Bright3stday <46619966+Bright3stday@users.noreply.github.com>
2021-06-25 19:06:03 +0800

    Add files via upload
    javascript practice utilising a basic login for user to enter and see their balance details

---
+++ hw1/script.js
@@ -0,0 +1,95 @@
+// (c) Anuflora Systems 
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('deposit');
+const money_minus = document.getElementById('loan');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const custname = document.getElementById('custname');
+
+const reco = document.getElementById('reco');
+const b1 = document.getElementById('b1');
+const b2 = document.getElementById('b2');
+
+
+const TransactionDataAll = [
+   { id: 1, customername: 'Flora', bank: 'DBS', deposit: 3000, loan: 2000 },
+   { id: 2, customername: 'Flora', bank: 'OCBC', deposit: 4000, loan: 2000 },
+   { id: 3, customername: 'Mikhil', bank: 'DBS', deposit: 3000, loan: 2000 },
+   { id: 4, customername: 'Sashil', bank: 'UOB', deposit: 6000, loan: 1000 },
+   { id: 5, customername: 'Jack', bank: 'UOB', deposit: 6000, loan: 8000 },
+   { id: 6, customername: 'Jill', bank: 'UOB', deposit: 7000, loan: 4000 },
+
+  ];
+
+ var TransactionData = null;
+
+// Add transactions to DOM list
+function addTransactionDOM(transaction) {
+  const balance_item = document.createElement('li');
+  custBalance = Math.abs(transaction.deposit) - Math.abs(transaction.loan); //calculates the customer balance 
+
+  (custBalance>0)? balance_item.classList.add('plus'):balance_item.classList.add('minus'); //ternary operator to check if cust has negative or positve balance and adds the relevant class to it
+  
+  //below uses ternary operator to output the correct message to the user depending on if they have positive or negative balance
+  (custBalance>0)? balance_item.innerHTML = `
+  ${transaction.customername}-${transaction.bank}  <span> $ ${Math.abs(
+    custBalance  
+  )}</span> 
+  `
+  :
+  balance_item.innerHTML = `
+  ${transaction.customername}-${transaction.bank}  <span> -$ ${Math.abs(
+    custBalance  
+  )}</span> 
+  `
+  ;
+
+  list.appendChild(balance_item);
+
+}
+
+// Update the balance, deposit and loan
+function updateValues() {
+  const deposits = TransactionData.map(transaction => transaction.deposit);
+  const loans = TransactionData.map(transaction => transaction.loan);
+  const total_deposit = deposits.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const total_loan = loans.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const bal = total_deposit - total_loan;
+  balance.innerText = `$${bal}`;
+  money_plus.innerText = `$${total_deposit}`;
+  money_minus.innerText = `$${total_loan}`;
+  reco.innerText = (bal >= 0)? "You Have Sound Financial Health": "Your Financial Health is Weak";
+}
+
+function init() {
+  
+  list.innerHTML = '';
+  list.innerHTML = '';
+  reco.innerHTML = '';
+  TransactionData = [...TransactionDataAll];
+  TransactionData.forEach(addTransactionDOM);
+  updateValues();
+}
+
+function filterTransaction(e) {
+  e.preventDefault();  //to prevent form from submitting and refreshing the page
+  list.innerHTML = '';
+  reco.innerHTML = '';
+  document.getElementById('yourBalance').innerHTML= "Your Balance"; //adds relevant text to html tag so that user can see relevant information after logging in
+  document.getElementById('depHeader').innerHTML= "Deposit";
+  document.getElementById('loanHeader').innerHTML= "Loan";
+  document.getElementById('details').innerHTML= "Balance Details";
+  document.getElementById('details').style.borderBottom= "1px solid #bbb";
+  document.getElementById('logIn').innerHTML = `Welcome ${custname.value.toUpperCase()}!`;
+  document.getElementById('addShadow').style.boxShadow= "var(--box-shadow)";
+  
+  TransactionData = TransactionDataAll.filter(tran => tran.customername.toUpperCase() == custname.value.toUpperCase());  
+  TransactionData.forEach(addTransactionDOM);
+  updateValues(); 
+  
+}
+
+
+//form.addEventListener('submit', filterTransaction);
+b1.addEventListener('click',filterTransaction);
+b2.addEventListener('click',init);  //no need to call init. when no event handler it will reload/referesh the page
